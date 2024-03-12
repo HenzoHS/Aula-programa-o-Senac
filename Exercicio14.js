af144eda13aea4ee4a4cfc3fdf1300c988fc346c
Henzo Henrique <147110604+HenzoHS@users.noreply.github.com>
2024-03-12 20:37:07 -0300

    Add files via upload

---
+++ Exercicio14.js
@@ -0,0 +1,7 @@
+let numero = parseFloat(prompt("Insira um numero"))
+
+if( numero > 10){
+    console.log("Seu numero é maior que 10")
+} else {
+    console.log("seu  numero é menor que 10")
+}
---
+++ Exercicio27.js
@@ -0,0 +1,9 @@
+let numero = parseFloat(prompt("insira o valor"))
+
+if(numero > 0) {
+    console.log("Positivo")
+} else if (numero < 0){
+    console.log('Negativo')
+} else {
+    console.log("Zero")
+}

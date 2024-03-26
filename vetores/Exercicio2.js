da484de9d25e6e3a374861fed945a66a1c7d48cb
Henzo Henrique <147110604+HenzoHS@users.noreply.github.com>
2024-03-26 20:59:05 -0300

    Add files via upload

---
+++ vetores/Exercicio2.js
@@ -0,0 +1,19 @@
+let veterinario ={
+    nome: "Henzo" ,
+    idade: "19" ,
+    animais: [ {
+            nome: "Eva" ,
+            tipo: "gato",
+             vaci: "É vacinado"
+    } , 
+            {
+            nome: "Jorge" ,
+            tipo: "Cachorro",
+            vaci: "Não é vacinado"
+            } ,
+            {
+                nome: "Claudio" ,
+                tipo: "Papagaio" , 
+                vaci: "Não é vacinado"
+            }]
+}

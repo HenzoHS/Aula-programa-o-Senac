96f000c42a4bb2c62cc2ed48177e0565e8b17c1b
Henzo Henrique <147110604+HenzoHS@users.noreply.github.com>
2024-04-15 21:12:42 -0300

    Update Exercicio68.js

--- vetores/Exercicio68.js
+++ vetores/Exercicio68.js
@@ -1,5 +1,10 @@
-let number = parseInt(prompt("Insira a quantidade de mercadorias no estoque"))
-let val = parseFloat(prompt("Insira o valor de mercadoria"))
-while(val <= 0){
-    val =parseFloat(prompt("Insira um valor valido"))
+let estoq = parseInt(prompt("Insira a quantidade de mercadorias em estoque"))
+let val = 0
+
+for(let i = 0 ; i < 3 ; i++){
+    val =+ parseFloat(prompt("Insira o valor dos produtos"))
 }
+
+let val_estoq = (estoq * val)
+let media = (val_estoq / estoq).toFixed(2)
+console.log("A quantidade de mercadoria em estoque é " , estoq , " e o valor medio das mercadorias em estoque é R$" , media)
